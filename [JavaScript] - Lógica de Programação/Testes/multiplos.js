c63421364c40c8f586383eac460fefad8fb7e1d7
Matheus Fontes <matheusfontes1921@gmail.com>
2023-06-27 09:50:38 -0300

    making better adjustments

--- [JavaScript] - Lógica de Programação/Testes/multiplos.js
+++ [JavaScript] - Lógica de Programação/Testes/multiplos.js
@@ -4,8 +4,17 @@ let numDois = 200;
 console.log(multiplos(numUm,numDois));
 
 function multiplos(numUm,numDois) {
+    let maior = 0;
+    let menor = 0;
+    if(numUm<numDois) {
+        maior = numDois;
+        menor = numUm;
+    } else {
+        maior = numUm;
+        menor = numDois;
+    }
     let soma = 0;
-    for (let i = numUm; i <= numDois; i++) {
+    for (let i = menor; i <= maior; i++) {
         if(i % 13 !== 0 ) {
         soma+=i;
         }
