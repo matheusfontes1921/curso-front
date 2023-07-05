319f747b6d8163cff6db54e9d8e879b9c33cbcfe
Matheus Fontes <matheusfontes1921@gmail.com>
2023-07-05 08:59:59 -0300

    string, number, boolean and any

---
+++ [TypeScript] - Iniciando projeto/src/aula-02.ts
@@ -0,0 +1,13 @@
+interface Pessoa {
+  nome: string;
+    idade: number;
+    maior: boolean;
+}
+const pessoa: Pessoa = {
+    nome: "Matheus",
+    idade: 20,
+    maior: true,
+
+};
+const number: number = 8;
+console.log(pessoa.idade);
