f2a79da95d5eb21f9858bd9548d8b8712abc8040
Matheus Fontes <matheusfontes1921@gmail.com>
2023-07-07 11:07:41 -0300

    useState

--- [React] - Montando Projeto/course-website/src/modules/login/screens/LoginScreen.tsx
+++ [React] - Montando Projeto/course-website/src/modules/login/screens/LoginScreen.tsx
@@ -8,7 +8,20 @@ import {
 import Input from "../../../shared/input/input/Input.tsx";
 import {TitleInput} from "../../../shared/input/input/input.styles";
 import Button from "../../../shared/button/button/Button";
+import {useState} from "react";
+import login from "../index";
 const LoginScreen = () => {
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('')
+    const handleUser = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUser(event.target.value);
+    }
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
+    const handleLogin = () => {
+        alert(`${user} + ${password}`)
+    }
     return (
         <ContainerLoginScreen>
             <BackgroudnImage src="/background.png" />
@@ -16,9 +29,9 @@ const LoginScreen = () => {
                 <LimitedContainer>
                 <LogoImage src="/emblemaCruzeiro.jpg" />
                     <TitleLogin level={2}>Login</TitleLogin>
-                    <Input title={"Usuário"}/>
-                    <Input title={"Senha"}/>
-                    <Button type="primary" margin="64px 0px 16px 0px">Entrar</Button>
+                    <Input title={"Usuário"} margin={"32px 0px 0px"} onChange={handleUser} value={user}/>
+                    <Input type="password" title={"Senha"} margin={"32px 0px 0px"} onChange={handlePassword} value={password}/>
+                    <Button type="primary" margin="64px 0px 16px 0px" onClick={handleLogin}>Entrar</Button>
                 </LimitedContainer>
             </ContainerLogin>
         </ContainerLoginScreen>
