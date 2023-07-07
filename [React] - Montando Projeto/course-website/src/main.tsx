61fef2fa5cfb6e7ee1a1a419ec6d8f2105c35650
Matheus Fontes <matheusfontes1921@gmail.com>
2023-07-07 15:37:25 -0300

    inserting root and login routes using react-router

--- [React] - Montando Projeto/course-website/src/main.tsx
+++ [React] - Montando Projeto/course-website/src/main.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import {createBrowserRouter, RouteObject, RouterProvider} from "react-router-dom";
+import { Router as RemixRouter } from "@remix-run/router";
+import LoginScreen from "./modules/login/screens/LoginScreen";
+import { loginRoutes } from "./modules/login/routes";
+export const rootRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <h1>Tela Principal</h1>,
+    errorElement: <h2>Erro</h2>,
+  },
+];
+
+const router: RemixRouter = createBrowserRouter([...rootRoutes, ...loginRoutes]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
