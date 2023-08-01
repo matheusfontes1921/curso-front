9056eeb8eecfa2ac58f10b68c488b65b7cafe872
Matheus Fontes <matheusfontes1921@gmail.com>
2023-08-01 09:23:10 -0300

    orders table and breadcrumb

--- [React] - Montando Projeto/course-website/src/modules/orders/screens/Order.tsx
+++ [React] - Montando Projeto/course-website/src/modules/orders/screens/Order.tsx
@@ -1,13 +1,49 @@
 import Screen from "../../../shared/screen/Screen";
-import {useOrder} from "../hooks/useOrder";
-
+import { useOrder } from "../hooks/useOrder";
+import { ColumnsType } from "antd/es/table";
+import { CategoryType } from "../../../shared/types/CategoryType";
+import { OrderType } from "../../../shared/types/OrderType";
+import Table from "../../../shared/table/Table";
+const columns: ColumnsType<OrderType> = [
+  {
+    title: "Id",
+    dataIndex: "id",
+    key: "id",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Data",
+    dataIndex: "data",
+    key: "data",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Usuário",
+    dataIndex: "user",
+    key: "user",
+    render: (_, target) => <a>{target.user?.name}</a>,
+  },
+  {
+    title: "Quantidade de produtos",
+    dataIndex: "amountProducts",
+    key: "amountProducts",
+    render: (text) => <a>{text}</a>,
+  },
+];
 const Order = () => {
-    const {orders} = useOrder();
+  const { orders } = useOrder();
   return (
-    <Screen>
-        {orders.map((order) => {
-            <div>{order.user}</div>
-        })}
+    <Screen
+      listBreadcrumb={[
+        {
+          name: "HOME",
+        },
+        {
+          name: "PEDIDOS",
+        },
+      ]}
+    >
+      <Table columns={columns} dataSource={orders} />
     </Screen>
   );
 };
